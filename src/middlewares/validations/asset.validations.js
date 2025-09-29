5498489a51d017c40c81f7b5815b6c76ff7e8dcc
NatZigaran. <natanaelzigaran@gmail.com>
2025-09-29 11:25:20 -0300

    Se entrega el examen

--- src/middlewares/validations/asset.validations.js
+++ src/middlewares/validations/asset.validations.js
@@ -1,3 +1,4 @@
 export const createAssetValidation = [
   // TODO: completar las validaciones para crear un recurso
+  // body("inventory_number").scape().unique().require(),
 ];
--- src/middlewares/validations/user.validations.js
+++ src/middlewares/validations/user.validations.js
@@ -1,6 +1,7 @@
 export const createUserValidation = [
   // TODO: completar las validaciones para crear un usuario
   body("username")
+    .scape()
     .notEmpty()
     .withMessage("El username es requerido")
     .isLength({ min: 3, max: 20 })
@@ -11,9 +12,17 @@ export const createUserValidation = [
     ),
 
   body("email")
+    .scape()
     .notEmpty()
     .withMessage("El email es requerido")
     .isEmail()
     .withMessage("Debe ser un email válido")
     .normalizeEmail(),
+  body("role").scape().notEmpty().isIn(["secretary", "administrator"]),
+
+  body("employee_number").scape().unique().require(),
+
+  body("first_name").scape().isLength({ min: 2, max: 50 }),
+
+  body("last_name").scape().isLength({ min: 2, max: 50 }),
 ];
