6a2e8dacba82045f87106af55e31c0b9bfd94a04
NatZigaran. <natanaelzigaran@gmail.com>
2025-09-29 09:51:21 -0300

    Se realizaron las referencias de assets y categori

--- src/models/mongoose/asset.model.js
+++ src/models/mongoose/asset.model.js
@@ -21,6 +21,17 @@ const AssetSchema = new Schema(
     acquisitionDate: { type: Date, required: true },
     acquisitionValue: { type: Number, required: true, min: 0 },
     // ! FALTA COMPLETAR ACA
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "El autor es requerido"],
+    },
+    category: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+      },
+    ],
   },
   { timestamps: true }
 );
